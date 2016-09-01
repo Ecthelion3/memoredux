451f60206edaa2aa9e3a16ef3bc2ed6b4ebcb04f
Wouter de Vos <wouter@codaisseur.com>
2016-09-01 11:16:45 +0200

    Set up Redux store and basic MaterialUI theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
 
-class App extends React.Component {
-    render() {
-        return (
-            <h1>Hello World!</h1>
-        );
-    }
+// Actions
+
+
+// Components
+
+// Material UI Components
+import mui from 'material-ui';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Material UI Colors
+import {
+  red700,
+  grey100, grey300, grey400, grey500,
+  white, darkBlack, fullBlack
+} from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: red700,
+    accent1Color: grey400,
+  }
+});
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <h1>Hello World</h1>
+      </MuiThemeProvider>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {}
+}
+
+App.propTypes = {}
+
+export default connect(mapStateToProps, {})(App)
